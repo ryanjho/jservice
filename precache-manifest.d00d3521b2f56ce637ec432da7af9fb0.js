587f10d9fa7d85e79cd6761b12b9fffacd9b5e32
Ryan Ho <ryanjho90@gmail.com>
2020-08-01 01:45:10 +0800

    Updates

---
+++ precache-manifest.d00d3521b2f56ce637ec432da7af9fb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654859a69f3e03f63b2f7ecd28df0281",
+    "url": "/jservice/index.html"
+  },
+  {
+    "revision": "9c1bd040393cadef7742",
+    "url": "/jservice/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5ede841a47868a9419d2",
+    "url": "/jservice/static/js/2.45e4a5f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jservice/static/js/2.45e4a5f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c1bd040393cadef7742",
+    "url": "/jservice/static/js/main.488c0354.chunk.js"
+  },
+  {
+    "revision": "afc594d2156ba0551eb6",
+    "url": "/jservice/static/js/runtime-main.e075a294.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jservice/static/media/logo.5d5d9eef.svg"
+  }
+]);
